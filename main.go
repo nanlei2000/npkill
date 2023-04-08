895d2b3abddb1ecf828b12544cd4814d872a849f
nanlei <1456958184@qq.com>
2023-04-09 03:09:11 +0800

    Add commands

--- main.go
+++ main.go
@@ -1,17 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path"
+	"path/filepath"
 
 	"github.com/nanlei2000/npkill/pkg/file"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	cwd, err := os.Getwd()
-	if err != nil {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "add a task to the list",
+				Action: func(cCtx *cli.Context) error {
+					p := cCtx.Args().First()
+					cwd, err := os.Getwd()
+					if err != nil {
+						log.Fatal(err)
+					}
+					if len(p) == 0 {
+						p = cwd
+					}
+
+					if !path.IsAbs(p) {
+						p = path.Join(cwd, p)
+					}
+
+					f := file.New(p)
+					f.FindNodeModules()
+
+					return nil
+				},
+			},
+			{
+				Name:    "del",
+				Aliases: []string{"d"},
+				Usage:   "complete a task on the list",
+				Action: func(cCtx *cli.Context) error {
+					p := cCtx.Args().First()
+					cwd, err := os.Getwd()
+					if err != nil {
+						log.Fatal(err)
+					}
+					if len(p) == 0 {
+						p = cwd
+					}
+					if !path.IsAbs(p) {
+						p = path.Join(cwd, p)
+					}
+					p = path.Clean(p)
+					if filepath.Base(p) != "node_modules" {
+						return nil
+					}
+
+					err = os.RemoveAll(p)
+					if err != nil {
+						log.Fatal(err)
+					}
+					fmt.Printf("Removed: %s\n", p)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-	f := file.New(cwd)
-	f.FindNodeModules()
 }
